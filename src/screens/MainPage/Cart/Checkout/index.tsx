907862e49c73f0dbce073c8240ad57a11d58f55a
Jihyun_Kim <jihyun3313@daum.net>
2024-07-08 01:50:30 +0900

    feat: 장바구니 우측 결제 섹션 퍼블리싱

--- src/screens/MainPage/Cart/Checkout/index.tsx
+++ src/screens/MainPage/Cart/Checkout/index.tsx
@@ -1,5 +1,33 @@
-const Checkout = () => {
-  return <div className={'w-[15rem] border-t-2 border-black'}>Checkout</div>;
+import { ReactElement } from 'react';
+
+import numToMoney from '@/utils/num-to-money';
+
+const Checkout = (): ReactElement => {
+  return (
+    <div className={'mb-auto w-[15rem] py-2 px-6 border-t-2 border-black bg-stone-100'}>
+      <div className={'text-sm font-bold flex justify-between mb-3'}>
+        <p>상품 금액</p>
+        <p>{numToMoney(10000)}원</p>
+      </div>
+
+      <div className={'text-xs text-sky-400 font-bold border-b-[1px] border-gray-200 flex justify-between pb-3 mb-3'}>
+        <p>VIP 할인</p>
+        <p>-10%</p>
+      </div>
+
+      <div className={'text-sm font-bold border-b-[1px] border-gray-200 flex justify-between pb-3 mb-3'}>
+        <p>배송비</p>
+        <p>0원</p>
+      </div>
+
+      <div className={'flex flex-col items-end mb-4'}>
+        <p className={'text-sm font-bold'}>예상 결제 금액</p>
+        <p className={'text-2xl font-bold text-red-600'}>{numToMoney(49905)}원</p>
+      </div>
+
+      <button className={'w-full p-4 text-sm font-bold text-white bg-red-600 mt-4 mb-4 '}>전체 상품 주문하기</button>
+    </div>
+  );
 };
 
 export default Checkout;
